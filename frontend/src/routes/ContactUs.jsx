9d3e0906ed531f4002e461905224cf92d89ce45c
Im-Pranshu <pranshu.sharma1303@gmail.com>
2025-05-16 23:05:30 +0530

    updated contact

--- frontend/src/routes/ContactUs.jsx
+++ frontend/src/routes/ContactUs.jsx
@@ -10,31 +10,36 @@ import "../css/ContactUs.css";
 const ContactUs = () => {
   return (
     <div className="contact-container">
+      <FaEnvelope className="contact-icon" />
       <h1 className="contact-title">Contact Us</h1>
       <p className="contact-description">
         We’re here to help! Whether you have questions, need support, or want to
         share feedback, feel free to reach out.
       </p>
-      <div className="contact-info">
+      <div className="contact-items">
         <div className="contact-item">
-          <FaEnvelope className="contact-icon" />
+          <FaEnvelope className="contact-item-icon" />
+          <h3>Email</h3>
           <p>
-            Email: <a href="mailto:support@clearq.com">support@clearq.com</a>
+            <a href="mailto:pranshu.sharma1303@gmail.com">support@clearq.com</a>
           </p>
         </div>
         <div className="contact-item">
-          <FaPhoneAlt className="contact-icon" />
+          <FaPhoneAlt className="contact-item-icon" />
+          <h3>Phone</h3>
           <p>
-            Phone: <a href="tel:+1234567890">+1 234 567 890</a>
+            <a href="tel:+91 7272010120">+91 7272****20</a>
           </p>
         </div>
         <div className="contact-item">
-          <FaMapMarkerAlt className="contact-icon" />
-          <p>Address: 123 ClearQ Lane, Productivity City, PQ 12345</p>
+          <FaMapMarkerAlt className="contact-item-icon" />
+          <h3>Address</h3>
+          <p>123 ClearQ Lane, Kanpur City, UP - India</p>
         </div>
         <div className="contact-item">
-          <FaHeadset className="contact-icon" />
-          <p>Support Hours: Mon-Fri, 9 AM - 6 PM</p>
+          <FaHeadset className="contact-item-icon" />
+          <h3>Support Hours</h3>
+          <p>Mon-Fri, 9 AM - 6 PM</p>
         </div>
       </div>
     </div>
